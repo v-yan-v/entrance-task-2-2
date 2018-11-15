8c33a3cde2b625412c329e58f15fd719cd8f5bba
Yan Vaidakovich <v.yan.v@yandex.ru>
2018-11-15 14:54:55 +0300

    html and styles for full-screen card floor temperature

--- src/styles/cards/cards.js
+++ src/styles/cards/cards.js
@@ -1,6 +1,4 @@
 //  todo: add html data tag with card-gadget type info
-//  todo:
-//  todo: html and styles for full-screen card floor temperature
 //  todo: html and styles for full-screen card lights
 //  todo: actions on events from full-screen cards (close, coloring active/changing slider, change data in compact mode)
 //  todo: actions on events from compact cards (open full-screen)
@@ -43,6 +41,45 @@ const cardAirTemperature = `<div class="Card--fullScreen">
   </div>
 </div>`;
 
+let raysDiv = '';
+for (let i=-45; i<=225; i+=3){
+  raysDiv += `<div class="gadget-Ray deg${i.toString()}"></div>`;
+}
+const cardFloorTemperature = `
+<div class="Card--fullScreen">
+  <div class="gadgetWrap">
+    <div class="gadget">
+      <div class="gadget-Header">
+        <div class="gadget-Title">
+          <span>HEADER TEXT</span>
+          <div class="gadget-Status">online</div>
+        </div>
+        <div class="gadget-Info">
+          <span class="gadget-InfoText">+22</span>
+          <div class="gadget-Icon">
+            <img src="./img/icons/icon_temperature.svg" alt="">
+          </div>
+        </div>
+      </div>
+
+      <div class="gadget-CircleControlBox">
+
+        <div class="gadget-Circle">
+          <div class="gadget-NewValue">+23</div>
+        </div>
+          
+        ${raysDiv}
+
+      </div>
+
+    </div>
+    <div class="gadget-ConfirmForm">
+      <button class="gadget-FormButtons active">apply</button>
+      <button class="gadget-FormButtons ">close</button>
+    </div>
+  </div>
+</div>
+`;
 
 function onCardClick(event) {
 }
