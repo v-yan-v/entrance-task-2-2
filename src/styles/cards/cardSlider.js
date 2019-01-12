8f1ac344f805a91b65b869ef5319b512f7146150
Yan Vaidakovich <v.yan.v@yandex.ru>
2019-01-12 17:27:08 +0300

    sliders move horizontal/vertical depending on width

--- src/styles/cards/cardSlider.js
+++ src/styles/cards/cardSlider.js
@@ -3,26 +3,34 @@
 export function sliderControl(sliderBox) {
   let slider = sliderBox.getElementsByClassName('gadget-Slider')[0];
   
-  
   slider.addEventListener('pointerdown', apd);
   slider.ondragstart = ()=>{ return false; };
   
+  let mediaQ = window.matchMedia("(min-width: 800px)");
   
-  // functions
+  mediaQ.addListener(()=>{
+    if (mediaQ.matches){
+      slider.style.top = '';
+    } else {
+      slider.style.left = '';
+    }
+  });
+  
+  // FUNCTIONS
   function apd(event) {
     // add pointerDown
   
-    let parentCoords = getCoords(sliderBox),
-        sliderCoords = getCoords(slider);
-        // indentX      = sliderCoords.left  - parentCoords.left,
-        // indentY      = sliderCoords.top - parentCoords.top;
+    let
+      parentCoords = getCoords(sliderBox),
+      sliderCoords = getCoords(slider);
     
-    let shift = { // distances from pointer event to slider edges
-      left:   event.pageX - sliderCoords.left,
-      top:    event.pageY - sliderCoords.top,
-      right:  sliderCoords.right  - event.pageX,
-      bottom: sliderCoords.bottom - event.pageY
-    };
+    let
+      shift = { // distances from pointer event to slider edges
+        left:   event.pageX - sliderCoords.left,
+        top:    event.pageY - sliderCoords.top,
+        right:  sliderCoords.right  - event.pageX,
+        bottom: sliderCoords.bottom - event.pageY
+      };
     
     moveAt(event);
     
@@ -31,25 +39,35 @@ export function sliderControl(sliderBox) {
     document.addEventListener('pointerup', rpm);
     
     function moveAt(e) {
-      let newIndent = {
-        left:   e.pageX - parentCoords.left - shift.left,
-        top:    e.pageY - parentCoords.top - shift.top,
-        right:  parentCoords.right - e.pageX - shift.right,
-        bottom: parentCoords.right - e.pageY - shift.bottom
-      };
-      let sliderWidth = sliderCoords.right - sliderCoords.left,
-          parentWidth = parentCoords.right - parentCoords.left;
-      
-      if (newIndent.left < 0){
+      let
+        newIndent = {
+          left:   e.pageX - parentCoords.left - shift.left,
+          top:    e.pageY - parentCoords.top - shift.top,
+          right:  parentCoords.right - e.pageX - shift.right,
+          bottom: parentCoords.bottom - e.pageY - shift.bottom
+        };
+  
+      if (mediaQ.matches){ // horizontal slider view
+        if (newIndent.left < 0) {
         newIndent.left = 0;
-      }
-      if (newIndent.right < 0){
-        newIndent.right = 0;
-        newIndent.left = parentWidth - sliderWidth;
+        }
+        if (newIndent.right < 0) {
+          newIndent.right = 0;
+          newIndent.left = parentCoords.width - sliderCoords.width;
+        }
+        slider.style.left = newIndent.left + 'px';
       }
       
-      slider.style.left = newIndent.left + 'px';
-      // slider.style.top  = parenCoords.top /*e.pageY - shiftY*/ + 'px';
+      if (!mediaQ.matches){ // vertical slider view
+        if (newIndent.top < 0){
+          newIndent.top = 0;
+        }
+        if (newIndent.bottom < 0){
+          newIndent.bottom = 0;
+          newIndent.top = parentCoords.height - sliderCoords.height;
+        }
+        slider.style.top  = newIndent.top + 'px';
+      }
     }
     
     function getCoords(elem) {   // кроме IE8-
@@ -58,7 +76,9 @@ export function sliderControl(sliderBox) {
         top: box.top + pageYOffset,
         left: box.left + pageXOffset,
         bottom: box.bottom + pageYOffset,
-        right: box.right + pageXOffset
+        right: box.right + pageXOffset,
+        width: box.width,
+        height: box.height
       };
     }
   
