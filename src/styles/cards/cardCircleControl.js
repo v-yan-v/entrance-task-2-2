ca3bf0e4fdb72010861c9b05492dd94e071dc5e6
Yan Vaidakovich <v.yan.v@yandex.ru>
2019-02-21 15:40:32 +0300

    circle slider(without change values)

--- src/styles/cards/cardCircleControl.js
+++ src/styles/cards/cardCircleControl.js
@@ -3,8 +3,11 @@
 export function circleControl(circleBox) {
   let rays = circleBox.getElementsByClassName('gadget-Ray');
   
-  circleBox.addEventListener('pointerdown', ()=>{
+  circleBox.addEventListener('click', setActiveRay);
+  
+  circleBox.addEventListener('pointerdown', (e)=>{
     circleBox.addEventListener('pointermove', setActiveRay);
+    setActiveRay(e);
   });
   circleBox.addEventListener('pointerup', ()=>{
     circleBox.removeEventListener('pointermove', setActiveRay);
@@ -13,10 +16,26 @@ export function circleControl(circleBox) {
     circleBox.removeEventListener('pointermove', setActiveRay);
   });
   
+  circleBox.addEventListener('touchstart', (e)=>{
+    circleBox.addEventListener('touchmove', setActiveRay);
+    setActiveRay(e);
+  });
+  circleBox.addEventListener('touchend', ()=>{
+    circleBox.removeEventListener('touchstart', setActiveRay);
+  });
+  circleBox.addEventListener('touchcancel', ()=>{
+    circleBox.removeEventListener('touchstart', setActiveRay);
+  });
+  
   function setActiveRay(e) {
-    if (e.target.classList.contains('gadget-Ray')) {
+    let target;
+    // use element under pointer as target if event is 'touchmove'
+    (e.type === 'touchmove') ? (target = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY)) : (target = e.target);
+    
+    if (target.classList.contains('gadget-Ray'))
+    {
       [].forEach.call(rays, (el) => {
-        if (el === e.target) {
+        if (el === target) {
           el.classList.add('active');
         } else {
           el.classList.remove('active');
