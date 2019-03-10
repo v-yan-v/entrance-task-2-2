9c60b30e081f8aeac536682e4c1f46cbb1d4f96d
Yan Vaidakovich <v.yan.v@yandex.ru>
2019-03-10 10:48:49 +0300

    nav buttons Forward and Back set active when scrolling

--- src/styles/common/Scroller.js
+++ src/styles/common/Scroller.js
@@ -2,6 +2,7 @@
 
 export function scroller(SectionNode) {
   let nav = SectionNode.getElementsByTagName('NAV')[0];
+  let navBtns = nav.getElementsByClassName('Nav-Item');
   let wrapper = SectionNode.getElementsByClassName('ScrollWrapper')[0];
   let itemsContainer = wrapper.firstElementChild;
   console.log(nav);
@@ -18,12 +19,35 @@ export function scroller(SectionNode) {
   
     if (navItem.classList.contains('linkForward')) {
       left -= wrapper.clientWidth / 3;
-      if (itemsContainer.scrollWidth + left < wrapper.clientWidth){ left = wrapper.clientWidth - itemsContainer.scrollWidth; }
+      
+      // сделать активными кнопки "назад" и "вперёд"
+      navBtns[0].classList.add('Nav-Item--active');
+      navBtns[navBtns.length-1].classList.add('Nav-Item--active');
+      
+      // запрещаем перематывать дальше чем хватает карточек
+      if (itemsContainer.scrollWidth + left < wrapper.clientWidth){
+        left = wrapper.clientWidth - itemsContainer.scrollWidth;
+        // сделать неактивным кнопку "вперёд"
+        navBtns[navBtns.length-1].classList.remove('Nav-Item--active');
+      }
+      
       itemsContainer.style.left = left + `px`;
     }
+    
+    
     if (navItem.classList.contains('linkBack')) {
       left += wrapper.clientWidth / 3;
-      if (left > 0){ left = 0; }
+      
+      // сделать активными кнопки "назад" и "вперёд"
+      navBtns[0].classList.add('Nav-Item--active');
+      navBtns[navBtns.length-1].classList.add('Nav-Item--active');
+      
+      //заперщаем перематывать вправо если дошли до начала карточек
+      if (left > 0){
+        left = 0;
+        // сделать неактивной кнопку "назад"
+        navBtns[0].classList.remove('Nav-Item--active');
+      }
       
       itemsContainer.style.left = left + `px`;
     }
