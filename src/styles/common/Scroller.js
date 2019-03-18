3568d38892c1b900747b2eec907146b960d161a5
Yan Vaidakovich <v.yan.v@yandex.ru>
2019-03-18 15:02:37 +0300

    +vertical scroll by buttons and swipe

--- src/styles/common/Scroller.js
+++ src/styles/common/Scroller.js
@@ -9,7 +9,7 @@ export function scroller() {
   let sections = document.getElementsByClassName('withScroll');
   
   [].forEach.call(sections, (section) =>{
-    let nav, navBtns, wrapper, itemsContainer, touchBegin, left;
+    let nav, navBtns, wrapper, itemsContainer, touchBegin, left, top;
     
     nav = section.getElementsByClassName('scrollNav')[0];
     wrapper = section.getElementsByClassName('scrollWrapper')[0];
@@ -19,7 +19,7 @@ export function scroller() {
     navBtns = nav.getElementsByClassName('Nav-Item');
     itemsContainer = wrapper.firstElementChild;
   
-    itemsContainer.style.transition = 'left .5s ease-out';
+    itemsContainer.style.transition = 'all .5s ease-out';
   
     // initialisation
     // сделать активными кнопки "назад" и "вперёд"
@@ -30,13 +30,16 @@ export function scroller() {
     nav.addEventListener('click', evt=>{
       let navItem = evt.target.closest('.Nav-Item');
       left = parseInt(getComputedStyle(itemsContainer).left);
+      top = parseInt(getComputedStyle(itemsContainer).top);
     
       if (navItem && navItem.classList.contains('linkForward')) {
         left -= wrapper.clientWidth / 3;
+        top -= wrapper.clientHeight / 2;
       }
     
       if (navItem && navItem.classList.contains('linkBack')) {
         left += wrapper.clientWidth / 3;
+        top += wrapper.clientHeight / 2;
       }
   
       setNewPos();
@@ -44,6 +47,7 @@ export function scroller() {
     
     section.addEventListener('touchstart', event=>{
       left = parseInt(getComputedStyle(itemsContainer).left);
+      top = parseInt(getComputedStyle(itemsContainer).top);
   
       touchBegin = {X: event.touches[0].clientX, Y: event.touches[0].clientY};
       itemsContainer.style.transition = '';
@@ -59,7 +63,18 @@ export function scroller() {
       touchBegin.Y = event.touches[0].clientY;
       
       left += shift.X;
+      top += shift.Y;
+  
+      // запрещаем двигать если по этой оси нет скрытого контента
+      if (itemsContainer.scrollHeight <= wrapper.clientHeight){
+        top = 0;
+      }
+      if (itemsContainer.scrollWidth <= wrapper.clientWidth){
+        left = 0;
+      }
+      
       itemsContainer.style.left = left + `px`;
+      itemsContainer.style.top = top + `px`;
     });
     
     section.addEventListener('touchend', event=>{
@@ -73,7 +88,7 @@ export function scroller() {
     });
     
     function setNewPos() {
-      itemsContainer.style.transition = 'left .5s ease-out';
+      itemsContainer.style.transition = 'all .5s ease-out';
   
       // запрещаем перематывать дальше чем хватает карточек
       if (itemsContainer.scrollWidth + left < wrapper.clientWidth){
@@ -82,8 +97,8 @@ export function scroller() {
         navBtns[navBtns.length-1].classList.remove('Nav-Item--active');
         // сделать активной кнопку "назад"
         navBtns[0].classList.add('Nav-Item--active');
-  
       }
+      
       //заперщаем перематывать вправо если дошли до начала карточек
       if (left > 0){
         left = 0;
@@ -92,8 +107,27 @@ export function scroller() {
         // сделат активной кнопку "вперёд"
         navBtns[navBtns.length-1].classList.add('Nav-Item--active');
       }
-  
+      
+      // запрещаем перематывать вниз
+      if (top > 0){
+        top = 0;
+        // сделать неактивной кнопку "назад"
+        navBtns[0].classList.remove('Nav-Item--active');
+        // сделат активной кнопку "вперёд"
+        navBtns[navBtns.length-1].classList.add('Nav-Item--active');
+      }
+      
+      // запрещаем перематывать вверх
+      if (itemsContainer.scrollHeight + top < wrapper.clientHeight){
+        top = wrapper.clientHeight - itemsContainer.scrollHeight;
+        // сделать неактивным кнопку "вперёд"
+        navBtns[navBtns.length-1].classList.remove('Nav-Item--active');
+        // сделать активной кнопку "назад"
+        navBtns[0].classList.add('Nav-Item--active');
+      }
+      
       itemsContainer.style.left = left + `px`;
+      itemsContainer.style.top = top + 'px';
     }
   });
 }
